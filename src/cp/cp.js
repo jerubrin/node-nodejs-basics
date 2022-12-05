82bbc656436b30ab1bb97d01fdfc0fe4d34c2798
Alexey Kuptsov <jerubbaal@mail.ru>
2022-12-05 23:55:10 +0600

    feat: remove wrong code

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -8,14 +8,7 @@ const spawnChildProcess = async (args) => {
     const file = path.join(__dirname, 'files', 'script.js');
     const command = `node`;
     const node = cp.spawn(command, [file].concat(args));
-    
-    node.on('data', data => process.stdout.write(data));
-    node.on('error', err => {console.log(`error: ${err.message}`)});
-    node.on('close', code => {
-        console.log(`childe process closed with code: ${code}`);
-        process.exit();
-    });
-
+    node.on('close', () => process.exit());
     node.stdout.pipe(process.stdin);
     process.stdout.pipe(node.stdin);
 };
